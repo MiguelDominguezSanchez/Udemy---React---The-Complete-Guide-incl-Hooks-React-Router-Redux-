7614ff2105f4ae2bc625e25fba41e4e0119a0639
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-08-30 07:57:27 +0200

    2 – 2. Understanding let and const

---
+++ custom.js
@@ -0,0 +1,14 @@
+// var myName = 'Max';
+// console.log(myName)
+
+// myName  = 'Manu';
+// console.log(myName)
+
+// const myName = 'Max'
+// console.log(myName)
+
+// myName = 'Manu'
+// console.log(myName)
+
+// Max custom.js:8:9
+// Uncaught TypeError: invalid assignment to const 'myName'
