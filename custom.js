f3364830a3c3b99240da47b4de2757035405a01e
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-08-30 08:29:15 +0200

    2 – 3. Arrow Functions

--- custom.js
+++ custom.js
@@ -12,3 +12,47 @@
 
 // Max custom.js:8:9
 // Uncaught TypeError: invalid assignment to const 'myName'
+
+// //// 3. Arrow Functions
+
+// function printMyName(name) {
+// 	console.log(name)
+// }
+
+// printMyName('Max')
+
+// const printMyName = (name) => {
+// 	console.log(name)
+// }
+
+// printMyName('Max')
+
+// // Omiting parentesis one argument
+
+// const printMyName = (name) => {
+// 	console.log(name)
+// }
+
+// printMyName('Max')
+
+// const printMyName = () => {
+// 	console.log('Max')
+// }
+
+// printMyName('Max')
+
+// const printMyName = (name, age) => {
+// 	console.log(name, age)
+// }
+
+// printMyName('Max', 28)
+
+// const multiply = (number) => {
+// 	return number * 2
+// }
+
+// console.log(multiply(2))
+
+// const multiply = (number) => number * 2
+
+// console.log(multiply(2))
