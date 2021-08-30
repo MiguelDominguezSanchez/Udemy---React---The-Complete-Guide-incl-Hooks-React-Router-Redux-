8656b8e089e5e2133179da328b7d12af421cc8ac
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-08-30 18:43:46 +0200

    2 – 7. The Spread & Rest Operator

--- custom.js
+++ custom.js
@@ -216,6 +216,11 @@ person.printGender()
 
 //
 
+{
+	/*
+
+
+
 class Human {
 	constructor() {
 		this.gender = 'male'
@@ -248,3 +253,334 @@ person.printGender()
 // so this ar classes, and classes are usd by react to create its components, at least this is one of two ways of creating components.
 // This is how you will see me using it in this course
 // And is important to understand that classes ar just blueprints for javascript objects, and are very comparable to constructor functions, where inheritance is comparable to prototypes.
+
+*/
+}
+
+// //// 7. The Spread & Rest Operator
+
+{
+	/*
+	We have learnt a lot about classes, 
+	and arrow functions.
+
+	Let's now tun our heads to new operators,
+	using the feature of javascript,
+	and we can already use,
+	the spread and the rest operator.
+
+	Actually is only one operator, three dots,
+	yes it may look strange,
+	but the operator is just three dots,
+	now if we call the spread || rest,
+	it depends on where we use it.
+
+	The spread operator:
+
+	'Used to split up array elements OR object properties'
+	we spread up an array || object.
+	For example if we have:
+
+	const newArray = [...oldArray, 1, 2]
+
+	An old array, 
+	and we want to add all the elements,
+	from  the old array to a new array,
+	and additionally to add one || two elements.
+	This fist syntax should b what we use
+	Three dots in front of 'oldArray'.
+
+	'...oldArray'
+
+	Will simply pull out the elements,
+	and add them to the new array,
+	which we create with the square brackets.
+	And all of course stand 
+	we can add mor elements to it,
+	since we just use the normal syntax,
+	with square brackets, to  create an array.
+
+	The same for th object:
+
+	const newObject = {...oldObject, newProps:5}
+
+	We create a new object with curly braces,
+	'newProp',
+	but we also have '...oldObject'
+	to pull out all the properties
+	of their values,
+	and add them as key value pairs
+	to the new object.
+	As a side note,
+	if the 'oldObject' also have
+	a new property,
+	it will be overwritten
+	by 'newProp:5' here,
+	so our new property takes presidency,
+	this is the 'Spread Operator'.
+
+	Now the 'Rest Operator',
+	is the same operator 
+	but used differently.
+
+	Here is:
+
+	'Used to merge a list of function arguments into an array.'
+
+	And this shows us where we use it,
+	we use it in a function argument list,
+	here is an example:
+
+	function sortArgs(...args) {
+		return args.sort()
+	}
+
+	'sortArgs' recibes an unlimited 
+	amount of arguments,
+	so one argument, two, three or whatever,
+	with '...', we write one argument '...args',
+	but we actually recibe more than one,
+	and they all be merged together into an array,
+	so then we can apply array methods
+	to our arguments list,
+	or do whatever we want to do,
+	with conveniently store arguments.
+
+	lets have a look to both usages in jsbin then:
+
+	*/
+}
+
+{
+	/*
+	Lets start with the Spread Operator:
+	
+*/
+}
+
+// We create an  Array of  numbers,
+// one, two, three, ...
+// add as many as you want
+
+// const numbers = [1, 2, 3]
+
+// now I want you to create a new array
+// 'newNumbers'
+
+// const newNumbers = []
+
+// and as you saw spread
+// is simply used by adding thr dots
+
+// const newNumbers = [...]
+
+// then the old array numbers
+
+// const newNumbers = [...numbers]
+
+// and then potentially,
+// you don't have to do that,
+// new numbers,
+// like '4'
+
+// const newNumbers = [...numbers, 4]
+
+// with that by 'console.log(newNumbers)' here
+
+// console.log(newNumbers)
+
+// and I hit run
+// we get:
+
+// const numbers = [1, 2, 3]
+// const newNumbers = [...numbers, 4]
+
+// console.log(newNumbers)
+
+// Array(4)[(1, 2, 3, 4)]
+
+// the old array with all its elements
+// and the new element,
+// and actually we don't add the new element,
+// if we were to do that without the dots,
+
+// const numbers = [1, 2, 3]
+// const newNumbers = [numbers, 4]
+
+// console.log(newNumbers)
+
+// it will be included as,
+// one element inside the new element
+
+// Array [ (3) […], 4 ]
+
+// it really pull out the old elements
+
+// const numbers = [1, 2, 3]
+// const newNumbers = [...numbers, 4]
+
+// console.log(newNumbers)
+
+//  as you can clearly see
+
+// Array(4)[(1, 2, 3, 4)]
+
+// That's the spread operator
+// and you wont see me use
+// this throughout this course
+// a couple of times
+// on both arrays and objects
+// to for example copy arrays
+// or add properties to an object
+// while safely copy the old object.
+
+// Again, I will obviously,
+// always mention why I am using
+// the spread operator when I do
+// throughout the course
+// just be aware that it exist
+
+// Let's now see the same for objects
+
+// I create a person object here
+// where I have a name,
+// let's say 'Max',
+
+// const person = {
+// 	name: 'Max',
+// }
+
+// and now I will have a new person object
+// which is a javascript object
+// a new one
+
+// const newPerson = {}
+
+// where first of all
+// use the spread operator on person
+
+// const newPerson = {
+// 	...person,
+// }
+
+// to copy all the property values pairs
+// of the old object
+
+// and potentially and that's all optional
+// add a new property
+
+// const person = {
+// 	name: 'Max',
+// }
+
+// const newPerson = {
+// 	...person,
+// 	age: 28,
+// }
+
+// and now if I console.log(newPerson)
+
+// console.log(newPerson)
+
+// You see:
+
+// Object { name: "Max", age: 28 }
+
+// so its taking the old person
+// distributing into the old person
+// that the  spread operator
+
+// Now the rest operator
+// which is used less though
+// is used in a function
+// and you can of course us the
+// ES6 javascript arrow function
+
+// so there we can have a filter function
+// or whatever you want to name it
+
+// const filter = ()
+
+// and we get a couple of args,
+// and you don't have to use args
+// we can also name it whatever you want
+// but you have to use the three dots in front of it
+
+// const filter = (...args)
+
+// so then there we can simply return,
+// and we could use the inline syntax in here,
+// to write it all in one line
+// without the return keyboard
+// we can return args
+
+// const filter = (...args) => {
+// 	return args.filter()
+// }
+
+// and then lets say
+// we call the dot & filter method
+// which is available on arrays
+
+// const filter = (...args) => {
+// 	// *1
+// 	return args.filter()
+// }
+
+// and keep in mind the dots in here *1
+// are used as a rest operator and
+// that merges the arguments into an array
+// so you can use array methods like filter,
+// filter will execute a function
+// on every element in the passed in array
+// so here we would get our element
+
+// const filter = (...args) => {
+// 	return args.filter(el)
+// }
+
+// and then
+// we can use an inline arrow function to simply say
+// return true || false
+// which filter expects
+// if the element is equal to one
+
+const filter = (...args) => {
+	return args.filter((el) => el === 1)
+}
+
+// now that might be a lot of syntax
+// that you don't build
+// three equal sings ===
+// for type and value equality
+// so the 'el' also has to be a number
+// and this is just an arrow function
+
+// (el) => el === 1
+
+// we passed to the built in filter method
+// so this has nothing to do with rest || spread
+// with that we could call filter
+// and let's console.log it to see something
+// console.log(filter)
+
+console.log(filter)
+
+// it's our filter function here
+// all I say is one, two, three
+
+console.log(filter(1, 2, 3))
+
+// and actually we should only log one year
+// as an array though
+
+// Array[1]
+
+// because we filter this array which is
+// created with a rest operator
+// with the three dots
+
+// so thats is rest && spread against
+
+// the usage of the three dots
+// you will see mo re often in this course
